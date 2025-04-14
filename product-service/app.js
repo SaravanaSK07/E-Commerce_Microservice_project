8b2f983bf5acd55d679b029ed1daf2c01eb9073c
Saravanakumar <sarangovind07@gmail.com>
2025-04-14 08:50:13 +0000

    Services are integreted commit on 2025-04-14 08:49:24

--- product-service/app.js
+++ product-service/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Product service!');
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
+ const app = express();
+ const port = 3000;
+ 
+ app.get('/products', (req, res) => {
+     res.json({ id: 101, name: "Laptop", price: 999 });
+ });
+ 
+ app.listen(port, () => {
+     console.log(`Product Service running on port ${port}`);
+ });
--- user-service/user-service/src/main/java/com/ecomm/UserController.java
+++ user-service/user-service/src/main/java/com/ecomm/UserController.java
@@ -1,12 +1,12 @@
 package com.ecomm;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-    @GetMapping("/users")
-    public String getUsers() {
-        return "Hello from User Service!";
-    }
-}
+ 
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RestController;
+ 
+ @RestController
+ public class UserController {
+     @GetMapping("/users")
+     public String getUsers() {
+         return "{\"id\": 1, \"name\": \"SK\"}";
+     }
+ }
